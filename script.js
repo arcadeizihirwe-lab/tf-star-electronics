563102286f627fa0ed84d74ec2e2927bc7668025
Arcade <arcadeizihirwe@gmail.com>
2026-07-09 12:06:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price){
+
+    cart.push({
+        name:name,
+        price:price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    alert(name + " has been added to your cart.");
+}
